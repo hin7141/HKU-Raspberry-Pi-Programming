66130ec505988e5d102e696c8f91db14a4110498
tinyauyu <yu.tin.yau@gmail.com>
2016-06-20 18:45:34 +0800

    lab3

--- python/rfcomm-server.py
+++ python/rfcomm-server.py
@@ -17,8 +17,8 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
 advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
-                   # service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   # profiles = [ SERIAL_PORT_PROFILE ], 
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
 #                   protocols = [ OBEX_UUID ] 
                     )
                    
